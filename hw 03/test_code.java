c06c009de4c2b9b0e479947c93693c65b4f71419
ax130885 <ax130885@gmail.com>
2025-03-18 11:57:51 +0800

    hw3 temp

--- hw 03/test_code.java
+++ hw 03/test_code.java
@@ -69,13 +69,48 @@ class member {
 }
 
 class DefenseSystem {
+
     public int[] result(int[] levels, int[] ranges) {
-        // Given the traits of each member and output
+        // Given the traits of each member levels,nd output
         // the leftmost and rightmost index of member
         // can be attacked by each member.
 
         // complete the code by returning an int[]
         // flatten the results since we only need an 1-dimentional array.
+
+        int[] result = new int[2 * levels.length];
+
+        // 遍歷每個塔
+        for (int i = 0; i < levels.length; i++) {
+
+            // 計算左右攻擊範圍
+            int left = -1;
+            int right = -1;
+
+            left = (i - ranges[i] < 0) ? 0 : i - ranges[i];
+            right = ((i + ranges[i]) > (levels.length - 1))? levels.length - 1 : i + ranges[i];
+
+            // 往左檢查
+            for (int j = i - 1; j >= left; j--) {
+                
+                if (levels[j] >= levels[i]) {
+                    left = j + 1;
+                    break;
+                }
+            }
+
+            // 往右檢查
+            for (int j = i + 1; j <= right; j++) {
+
+                if (levels[j] >= levels[i]) {
+                    right = j - 1;
+                    break;
+                }
+
+        }
+
+        return result;
+
     }
 
     public static void main(String[] args) {
