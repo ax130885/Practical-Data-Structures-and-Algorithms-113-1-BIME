b65518e37b85632f4f22757c74aace3809d4b2e5
ax130885 <ax130885@gmail.com>
2025-04-04 02:19:21 +0800

    hw 05 finish

--- README.md
+++ README.md
@@ -6,8 +6,20 @@
 hw01(Union): https://hackmd.io/@CiqLOooyRwWmK--mMkfetA/SyieYHDP1l  
 hw02(動態規劃 DP): https://hackmd.io/@CiqLOooyRwWmK--mMkfetA/B1BlzSwFJe  
 hw03(單調棧 Monotonic Stack): https://hackmd.io/@CiqLOooyRwWmK--mMkfetA/S1edebLt5Jx  
+Supplement 03_1: 分別用 Array 與 Linked List 實作 Stack, Queue. 矩陣實作的版本需要可以動態增減.  
+Supplement 03_2: 循環鏈表, 雙向鏈表.  
 hw04(凸殼 Convex Hull): https://hackmd.io/@CiqLOooyRwWmK--mMkfetA/HkzhEbmskl  
 hw05: https://hackmd.io/@CiqLOooyRwWmK--mMkfetA/BJ7EdE2jye  
+Supplement 05_1: 實作 Knuth Shuffle, Bubble Sort, Selection Sort, Insertion Sort, Shell Sort (分治的 Insert sort).  
+Supplement 05_2: 實作 Merge Sort, Quick Sort (Median of 3), 3 Way Quick Sort.  
+Supplement 05_3: 實作 Heap Sort.  
+
+考慮用哪種 sort 應該要考慮
+1. 穩定性:在遇到相同大小時不能交換，並且不可破壞上次排序的結果。通常只要不是鄰近兩兩交換的，都不穩定。(只有merge, insert sort是穩定)
+2. 確定性:有一致的執行過程。快速排序因為一開始會洗牌，所以沒有確定性。
+3. 平行度:快速排序無法平行化
+4. 空間花費:merge sort需要額外空間保存分治過程，無法直接交換。
+![alt text](image.png)
 
 作業繳交網址:
 https://c4lab.bime.ntu.edu.tw:13000/
---
+++ hw 05/解答 test_code.java
@@ -0,0 +1,117 @@
+import java.util.*;
+import java.io.FileReader;
+import java.io.IOException;
+// import org.json.simple.JSONArray;
+// import org.json.simple.JSONObject;
+// import org.json.simple.parser.JSONParser;
+// import org.json.simple.parser.ParseException;
+
+// class test {
+//     public static void test(String filename) {
+//         JSONParser parser = new JSONParser();
+//         try (FileReader reader = new FileReader(filename)) {
+//             JSONArray allCases = (JSONArray) parser.parse(reader);
+//             for (Object obj : allCases) {
+//                 JSONObject groupObj = (JSONObject) obj;
+//                 int groupNum = ((Long) groupObj.get("case")).intValue();
+//                 JSONArray dataArray = (JSONArray) groupObj.get("data");
+//                 int groupPass = 0;
+//                 System.out.println("Case " + groupNum + ":");
+//                 int subCaseIndex = 1;
+//                 for (Object dataObj : dataArray) {
+//                     JSONObject testcase = (JSONObject) dataObj;
+//                     JSONArray numsArr = (JSONArray) testcase.get("nums");
+//                     int[] nums = new int[numsArr.size()];
+//                     for (int i = 0; i < numsArr.size(); i++) {
+//                         nums[i] = ((Long) numsArr.get(i)).intValue();
+//                     }
+//                     int expectedCount = ((Long) testcase.get("RebelsCount")).intValue();
+
+//                     int actualCount = new Rebels().CountofRebels(nums);
+
+//                     boolean countCorrect = (expectedCount == actualCount);
+
+//                     System.out.println("  Subcase " + subCaseIndex + ": " + (countCorrect ? "AC" : "WA"));
+//                     if (countCorrect) {
+//                         groupPass++;
+//                     }
+//                     subCaseIndex++;
+//                 }
+//                 System.out.println("  Score: " + groupPass + " / " + dataArray.size());
+//             }
+//         } catch (IOException | ParseException e) {
+//             e.printStackTrace();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         test("test_cases_public.json");
+//     }
+// }
+
+// 題目要求: 檢查左邊的元素>2*右邊元素的次數
+// 解題想法: 相當於merge sort，只是在合併階段，額外增加了一個if，來統計滿足 nums[i] > 2 * nums[j]的次數
+class Rebels {
+    public static int CountofRebels(int[] nums) {
+        return mergeSortAndCount(nums, 0, nums.length - 1);
+    }
+
+    private static int mergeSortAndCount(int[] nums, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            // 遞歸處理左半部分和右半部分，並累加各自的計數
+            count += mergeSortAndCount(nums, left, mid);
+            count += mergeSortAndCount(nums, mid + 1, right);
+            // 合併並統計當前階段的計數
+            count += mergeAndCount(nums, left, mid, right);
+        }
+        return count;
+    }
+
+    private static int mergeAndCount(int[] nums, int left, int mid, int right) {
+        // 創建左右子數組的副本，範圍為 [left, mid] 和 [mid+1, right]
+        int[] leftArr = Arrays.copyOfRange(nums, left, mid + 1);
+        int[] rightArr = Arrays.copyOfRange(nums, mid + 1, right + 1);
+
+        int count = 0; // 統計當前階段的Rebels數量
+        int p = 0; // 右子數組的指針，用於統計Rebels對
+
+        // 計算左子數組中的元素大於右子數組元素兩倍的對數
+        for (int q = 0; q < leftArr.length; q++) {
+            // 找到右子數組中第一個不滿足 leftArr[q] > 2 * rightArr[p] 的位置
+            while (p < rightArr.length && leftArr[q] > 2 * rightArr[p]) {
+                p++;
+            }
+            // 累加滿足條件的元素數量
+            count += p;
+        }
+
+        // 合併兩個已排序的子數組到原數組中
+        int i = 0, j = 0, k = left;
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                nums[k++] = leftArr[i++];
+            } else {
+                nums[k++] = rightArr[j++];
+            }
+        }
+
+        // 處理剩餘元素
+        while (i < leftArr.length) {
+            nums[k++] = leftArr[i++];
+        }
+        while (j < rightArr.length) {
+            nums[k++] = rightArr[j++];
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // Test case
+        int[] nums = { 1, 3, 2, 3, 1 };
+        int count = CountofRebels(nums);
+        System.out.println("Rebels Count: " + count);
+    }
+}
